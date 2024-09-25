90dd285e52eb29405fe322c90fa9a26f2ec14c8e
Daniil <129774580+Daniil-l17@users.noreply.github.com>
2024-09-25 19:00:53 +0500

    Update concat.js

--- concat.js
+++ concat.js
@@ -1,4 +1,4 @@
-Array.prototype.myConcat = function customConcat(array) {
+Array.prototype.myConcat = function (array) {
   if (!Array.isArray(array)) {
     throw new Error('должен быть масивом')
   }
